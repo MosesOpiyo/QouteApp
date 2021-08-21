d6499c19ad0a558db8e9c402982820cd8a98d33b
Moses <moses.junior@student.moringaschool.com>
2021-08-21 22:40:40 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QouteComponent } from './qoute/qoute.component';
+import { QouteNavbarComponent } from './qoute-navbar/qoute-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QouteComponent,
+    QouteNavbarComponent
   ],
   imports: [
     BrowserModule,
