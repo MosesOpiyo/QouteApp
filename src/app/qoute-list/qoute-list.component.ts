019c42e10a40be7738c838bbc11c117ca8351358
Moses <moses.junior@student.moringaschool.com>
2021-08-22 15:40:45 +0300

    changes

--- src/app/qoute-list/qoute-list.component.ts
+++ src/app/qoute-list/qoute-list.component.ts
@@ -6,12 +6,22 @@ import { Qoute } from '../qoute';
   templateUrl: './qoute-list.component.html',
   styleUrls: ['./qoute-list.component.css']
 })
-export class QouteListComponent  {
+export class QouteListComponent implements OnInit {
   qoutes :Qoute[] = [
    new Qoute (1,'Seek and you will find','make the effort and you will see results'),
    new Qoute(2,'where there is a will,there is a way','nothing is impossible with the right effort'),
     new Qoute(3,'Dont cry over split milk','theres no need crying over past mistakes')
   ]
+  toggleDetails(index) {
+    this.qoutes[index].showDescription = !this.qoutes[index].showDescription;
+
+  }
+
+  constructor() {}
+
+  ngOnInit () {
+
+  }
       
   
   
